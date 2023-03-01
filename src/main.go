1973e32ef90b2c4e2f03cb4cf6d9ec77a58e399b
kingschan <kings.chan@qq.com>
2023-03-01 21:17:36 +0800

    first version

--- src/main.go
+++ src/main.go
@@ -1,16 +1,65 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"github.com/robfig/cron"
+	dnspod "github.com/tencentcloud/tencentcloud-sdk-go/tencentcloud/dnspod/v20210323"
+	"log"
 	"my-ddns/src/myapi"
 	"my-ddns/src/myconf"
 )
 
+// 记录需要修改域名的最新状态
+var item dnspod.RecordListItem
+
 func main() {
-	fmt.Println(myapi.GetInterNetIp())
-	list, _ := myapi.RecordList(myconf.App.SecretId, myconf.App.SecretKey, myconf.App.Domain)
-	for i := 0; i < len(list); i++ {
-		fmt.Printf("\n %s %s %v", *list[i].Name, *list[i].Value, *list[i].RecordId)
+	modifyTask()
+	// 新建一个定时任务对象
+	// 根据cron表达式进行时间调度，cron可以精确到秒，大部分表达式格式也是从秒开始。
+	//crontab := cron.New()  默认从分开始进行时间调度
+	crontab := cron.New(cron.WithSeconds()) //精确到秒
+	//定义定时器调用的任务函数
+	task := func() {
+		modifyTask()
+		//fmt.Println("hello world", time.Now())
+	}
+	//定时任务
+	//cron表达式
+	spec := "*/60 * * * * ?"
+	// 添加定时任务,
+	crontab.AddFunc(spec, task)
+	// 启动定时器
+	crontab.Start()
+	// 定时任务是另起协程执行的,这里使用 select 简答阻塞.实际开发中需要
+	// 根据实际情况进行控制
+	select {} //阻塞主线程停止
+
+}
+
+// 更新任务
+func modifyTask() {
+	if item.RecordId == nil {
+		log.Println("获取域名记录列表")
+		list, _ := myapi.RecordList(myconf.App.SecretId, myconf.App.SecretKey, myconf.App.Domain)
+		for i := 0; i < len(list); i++ {
+			b, _ := json.Marshal(*list[i])
+			fmt.Printf("%s \n", b)
+			if *list[i].Name == myconf.App.Target {
+				item = *list[i]
+			}
+		}
 	}
 
+	if item.RecordId == nil {
+		log.Print("未找到要修改的记录！")
+		return
+	}
+
+	ip := myapi.GetInterNetIp()
+	if *item.Value == ip {
+		log.Print("本机公网Ip: ", ip, " -> 地址未改变,无需修改！")
+		return
+	}
+	myapi.ModifyIp(myconf.App.SecretId, myconf.App.SecretKey, myconf.App.Domain, myconf.App.Target, ip, *item.RecordId)
 }
--- src/myapi/dnspod.go
+++ src/myapi/dnspod.go
@@ -6,6 +6,7 @@ import (
 	"github.com/tencentcloud/tencentcloud-sdk-go/tencentcloud/common/errors"
 	"github.com/tencentcloud/tencentcloud-sdk-go/tencentcloud/common/profile"
 	dnspod "github.com/tencentcloud/tencentcloud-sdk-go/tencentcloud/dnspod/v20210323"
+	"log"
 )
 
 // 获取主域名下的所有解析记录
@@ -25,9 +26,7 @@ func RecordList(sid string, skey string, domain string) (list []*dnspod.RecordLi
 
 	// 实例化一个请求对象,每个接口都会对应一个request对象
 	request := dnspod.NewDescribeRecordListRequest()
-
 	request.Domain = common.StringPtr(domain)
-
 	// 返回的resp是一个DescribeRecordListResponse的实例，与请求对象对应
 	response, err := client.DescribeRecordList(request)
 	if _, ok := err.(*errors.TencentCloudSDKError); ok {
@@ -47,13 +46,17 @@ func RecordList(sid string, skey string, domain string) (list []*dnspod.RecordLi
 	return response.Response.RecordList, nil
 }
 
-func update() {
-	// 实例化一个认证对象，入参需要传入腾讯云账户 SecretId 和 SecretKey，此处还需注意密钥对的保密
-	// 代码泄露可能会导致 SecretId 和 SecretKey 泄露，并威胁账号下所有资源的安全性。以下代码示例仅供参考，建议采用更安全的方式来使用密钥，请参见：https://cloud.tencent.com/document/product/1278/85305
-	// 密钥可前往官网控制台 https://console.cloud.tencent.com/cam/capi 进行获取
+// 更新域名记录
+// sid : tencent SecretId
+// skey : tencent SecretKey
+// domain : 一级域名
+// target : 要修改的二级域名，如果是一级域名传@
+// ip : 本机的公网地址
+// id : 要修改的二级域名的RecordId
+func ModifyIp(sid string, skey string, domain string, target string, ip string, id uint64) {
 	credential := common.NewCredential(
-		"SecretId",
-		"SecretKey",
+		sid,
+		skey,
 	)
 	// 实例化一个client选项，可选的，没有特殊需求可以跳过
 	cpf := profile.NewClientProfile()
@@ -65,8 +68,12 @@ func update() {
 	request := dnspod.NewModifyRecordRequest()
 
 	////////////////
-
-	//
+	request.Domain = common.StringPtr(domain)
+	request.SubDomain = common.StringPtr(target)
+	request.RecordType = common.StringPtr("A")
+	request.RecordLine = common.StringPtr("默认")
+	request.Value = common.StringPtr(ip)
+	request.RecordId = common.Uint64Ptr(id)
 	// 返回的resp是一个ModifyRecordResponse的实例，与请求对象对应
 	response, err := client.ModifyRecord(request)
 	if _, ok := err.(*errors.TencentCloudSDKError); ok {
@@ -77,5 +84,5 @@ func update() {
 		panic(err)
 	}
 	// 输出json格式的字符串回包
-	fmt.Printf("%s", response.ToJsonString())
+	log.Print("修改完成：%s", response.ToJsonString())
 }
