0cca0cddab0e4b86efc5d39593ed197dd3848120
kingschan <kings.chan@qq.com>
2023-03-24 21:41:26 +0800

    get my public ip url

--- src/myapi/myip.go
+++ src/myapi/myip.go
@@ -8,6 +8,7 @@ import (
 //获取本机公网ip的地址
 //https://ipinfo.io/ip
 //https://myexternalip.com/raw
+//http://httpbin.org/ip
 func GetInterNetIp() string {
 	resp, err := http.Get("https://myexternalip.com/raw")
 	if err != nil {
